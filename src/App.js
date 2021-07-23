3bd2ac5dabe9f41fce340edfa25d4e308fbefabf
Laura Cole <lauracole1900@comcast.net>
2021-07-22 19:14:54 -0600

    added state vars

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import { Game } from "./components"
 import './App.css';
 
 function App() {
   return (
     <>
-
+      <Game />
     </>
   );
 }
--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 const Game = () => {
+  const [tickTime, setTickTime] = useState(200);
+  const [rows, setRows] = useState(30);
+  const [cols, setCols] = useState(30);
+  const [grid, setGrid] = useState([]);
+  const [food, setFood] = useState({});
+  const [snake, setSnake] = useState({
+    head: {},
+    tail: []
+  })
+  const [direction, setDirection] = useState("right");
+  const [dead, setDead] = useState(false);
 
 
   return(
